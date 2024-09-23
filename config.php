a2aac73326db6ff6c2aa033f8e798281352ff3c6
Spencer Login <spencerlogin03@gmail.com>
2024-09-22 23:44:37 -0400

    PHP connection to database

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// TODO: find out the MySQL server configuration so connection can be possible
+// configuration for aptitude server
+const DB_HOST = 'localhost';
+const DB_NAME = 'slogin_db';
+const DB_USERNAME = 'slogin';
+const DB_PASSWORD = ''; // person number
