d48259867c099385f5754a0b21dfaf3df997f675
Spencer Login <spencerlogin03@gmail.com>
2024-09-24 15:22:24 -0400

    Move database connection logic to one file

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
-require_once 'config.php';
-require_once 'connect.php';
+const DB_HOST = 'localhost';
+const DB_NAME = 'slogin_db';
+const DB_USERNAME = 'slogin';
+const DB_PASSWORD = ''; // person number
 
-$db = connect(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
+$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
+if($db->connect_error) {
+    die("Cannot connect to database: \n" . $db->connect_error . "\n" . $db->connect_errno);
+}
+    
 if($db instanceof mysqli) {
     echo "Client info: " . $db->client_info . "\n";
     echo "Client version: " . $db->client_version;
