245c80e83fc2dcb1658bae4fbc2b15f6a97709ac
Spencer Login <spencerlogin03@gmail.com>
2024-09-29 22:45:00 -0400

    Filled in error handling for http requests or non-POST requests

--- api/signup.php
+++ api/signup.php
@@ -1,21 +1,35 @@
 <?php
-    // TODO: verify that the request was sent using HTTPS
-    if(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on') {
-        // TODO: handle case where HTTPS isn't being used
-        // send bad response with details
+    // send responses in JSON
+    header("Content-Type: application/json");
+
+    // verify that the request was sent using HTTPS
+    if(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on') {        
+        $http_response_code(403);
+        $errorResponse = [
+            'status' = 'Forbidden',
+            'message' = 'HTTP used for request, use HTTPS instead',
+        ];
+        echo json_encode($errorResponse);
     }
 
-    // TODO: verify that the request method is POST
+    // verify that the request method is POST
     if($_SERVER['REQUEST_METHOD'] != 'POST') {
-        // TODO: handle case where the request is not a POST request
-        // send bad response with details
+        $http_response_code(405);
+        $errorResponse = [
+            'status' = 'Method not allowed',
+            'message' = 'Method other than POST used, use POST instead',
+        ];
+        echo json_encode($errorResponse);
     }
 
-    // TODO: get data from request
+    // get data from request
     $data = json_decode(file_get_contents("php://input"));
 
     // TODO: check that all data is valid
+    // Note: should be done in frontend before request is sent, so ignore for now
 
     // TODO: query the database to check that the username is not already being used
-    
+
     // TODO: query the database to check that the email is not already being used
+
+    // TODO: check for cookies
