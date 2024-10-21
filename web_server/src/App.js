638a59e302d51d5bb0e9a90bca4794a153f210aa
MrDerpypants <gffajard@buffalo.edu>
2024-10-21 19:23:17 -0400

    This should be fixed now. Changed dashboard to be able to complie.

--- web_server/src/App.js
+++ web_server/src/App.js
@@ -8,7 +8,7 @@ import AccountPage from './AccountPage';
 
 function App() {
   return (
-    <Router basename="/CSE442/2024-Fall/slogin">
+    <Router basename="/CSE442/2024-Fall/gffajard">
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
